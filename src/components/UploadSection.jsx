d2a1f673885a6b9a82803c7e15c4421a902dac97
Dreiii18 <andreiartap@gmail.com>
2024-11-29 22:15:34 -0800

    .

--- src/components/UploadSection.jsx
+++ src/components/UploadSection.jsx
@@ -90,7 +90,7 @@ function UploadSection({ onNewPost }) {
         <div>
             {/* POPOUT */}
             <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" ref={modalRef} data-bs-backdrop="true">
-                <div className="modal-dialog">
+                <div className="modal-dialog modal-dialog-centered">
                     <div className="modal-content">
                         <div className="modal-header">
                             <h1 className="modal-title fs-5 text-center w-100" id="exampleModalLabel">Create post</h1>
